de3ce3e4cdf0b81e6390c4e3b83e4107736924fe
lsoncini <lsoncini@itba.edu.ar>
2016-09-18 00:45:46 -0300

    Main Menu First Idea Implemented.

--- ResellingApp/app/src/main/java/raptor/me/resellingapp/activity/MainActivity.java
+++ ResellingApp/app/src/main/java/raptor/me/resellingapp/activity/MainActivity.java
@@ -1,15 +1,61 @@
 package raptor.me.resellingapp.activity;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
 
+import butterknife.BindView;
 import raptor.me.resellingapp.R;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
+    @BindView(R.id.purchasesButton) LinearLayout purchases;
+    @BindView(R.id.salesButton) LinearLayout sales;
+    @BindView(R.id.statsButton) LinearLayout stats;
+    @BindView(R.id.clientsButton) LinearLayout clients;
+    @BindView(R.id.settingsButton) LinearLayout settings;
+    @BindView(R.id.productsButton) LinearLayout products;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.fragment_main);
+
+        purchases.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                //Intent intent = new Intent(MainActivity.this, PurchasesActivity.class);
+                //startActivity(intent);
+            }
+        });
+        sales.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                //Intent intent = new Intent(MainActivity.this, SalesActivity.class);
+                //startActivity(intent);
+            }
+        });
+        stats.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                //Intent intent = new Intent(MainActivity.this, StatsActivity.class);
+                //startActivity(intent);
+            }
+        });
+        clients.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                //Intent intent = new Intent(MainActivity.this,ClientsActivity.class);
+                //startActivity(intent);
+            }
+        });
+        products.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                //Intent intent = new Intent(MainActivity.this,ProductListActivity.class);
+                //startActivity(intent);
+            }
+        });
+        settings.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                //Intent intent = new Intent(MainActivity.this,SettingsActivity.class);
+                //startActivity(intent);
+            }
+        });
     }
 }
